912b81dc4b3177f57b1e7fe081ae8b8e84c0023a
cris.dux <crisfer.4217@gmail.com>
2022-03-14 18:55:25 -0400

    listo el nav para moviles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
 // Grab elements
-
+const selectElement = selector => {
+    const element = document.querySelector(selector);
+    if(element) return element;
+    throw new Error (`Asegurese de que ${selector} exista y este bien escrito`);
+}
 //Nav styles on scroll
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if(this.scrollY >= 15){
+        headerElement.classList.add('activated')
+    }else{
+        headerElement.classList.remove('activated')
+    }
+}
 
+window.addEventListener('scroll', scrollHeader)
 // Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
 
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+}   
+menuToggleIcon.addEventListener('click', toggleMenu);
 // Open/Close search form popup
 
 // -- Close the search form popup on ESC keypress
